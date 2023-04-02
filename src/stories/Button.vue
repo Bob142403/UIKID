bf38739d60080cd1aaac53e77b6190f2a193519b
Bob142403 <boboshka2006@mail.ru>
2023-04-02 16:58:30 +0500

    Test

--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -14,11 +14,8 @@ import '../index.css'
 import './button.css'
 import { reactive, computed } from 'vue'
 
-export default { //asdadaasdasdasdas
-  //asdasdsa
-  //asdasdas
-  //asdadsa
-  name: 'my-button',
+export default {
+  name: 'button',
 
   props: {
     size: {
@@ -40,18 +37,14 @@ export default { //asdadaasdasdasdas
       classes: computed(() => ({
         'bg-purple-700 hover:bg-purple-800 focus:ring-purple-300 text-white':
           props.color === 'purple',
-        'text-white bg-red-700 hover:bg-red-800 focus:ring-red-300': 
-          props.color === 'red',
-        'text-white bg-green-700 hover:bg-green-800  focus:ring-green-300': 
-          props.color === 'green',
+        'text-white bg-red-700 hover:bg-red-800 focus:ring-red-300': props.color === 'red',
+        'text-white bg-green-700 hover:bg-green-800  focus:ring-green-300': props.color === 'green',
         'text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-300':
           props.color === 'yellow',
         'text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-gray-200':
           props.color === 'light',
-        'text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300': 
-          props.color === 'blue',
-        'text-white bg-gray-800 hover:bg-gray-900 focus:ring-gray-300': 
-          props.color === 'dark',
+        'text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300': props.color === 'blue',
+        'text-white bg-gray-800 hover:bg-gray-900 focus:ring-gray-300': props.color === 'dark',
         [`storybook-button--${props.size || 'medium'}`]: true
       })),
       onClick() {
